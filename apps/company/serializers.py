a6778f349505938bce1bf45a90f54841b1da24e7
mukhammmad-irmatov <mukhammad.irmatov@gmail.com>
2024-10-27 14:57:35 +0500

    company serializer

--- apps/company/serializers.py
+++ apps/company/serializers.py
@@ -1,3 +1,10 @@
 from rest_framework import serializers
 
 from apps.company.models import Company
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = "__all__"
+        read_only_fields = ("id",)
