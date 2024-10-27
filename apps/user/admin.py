3613f3843eedf7aeb8525f1e398d288d0123016e
mukhammmad-irmatov <mukhammad.irmatov@gmail.com>
2024-10-27 17:02:35 +0500

    add user to django admin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from apps.user.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "email", "phone_number", "user_role")
+    search_fields = ("username", "first_name", "last_name", "email", "phone_number")
+    list_filter = ("user_role",)
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email", "phone_number")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "password1", "password2"),
+        }),
+    )
+    ordering = ("username",)
+    filter_horizontal = ("groups", "user_permissions")
+    readonly_fields = ("last_login", "date_joined")
+    list_per_page = 10
+    actions = ["make_verified"]
+
+    def make_verified(self, request, queryset):
+        queryset.update(is_verified=True)
+    make_verified.short_description = "Mark selected users as verified"
