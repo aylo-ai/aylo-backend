2c6a05314efc34a478ec3b65c505106a069fb063
mukhammmad-irmatov <mukhammad.irmatov@gmail.com>
2024-11-02 19:41:42 +0500

    change assistant url to chat

--- apps/urls.py
+++ apps/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("payment/", include(("payment.urls", "payment"), namespace="payment")),
     path("company/", include(("company.urls", "company"), namespace="company")),
     path("integration/", include(("integration.urls", "integration"), namespace="integration")),
-    path("assistant/", include(("assistant.urls", "assistant"), namespace="assistant")),
+    path("chat/", include(("assistant.urls", "assistant"), namespace="assistant")),
 
 ]
