0c274a3ec00d01a0d03659d0dfe10c86fe0a81b9
mukhammmad-irmatov <mukhammad.irmatov@gmail.com>
2025-01-07 21:54:40 +0500

    referal manbay -> bizni qayerdan eshitdi

--- apps/shared/addons/telegram.py
+++ apps/shared/addons/telegram.py
@@ -8,7 +8,7 @@ from shared.addons.validations import error_response
 
 USER_INFO_REGEX = r"#registered_user_info\s*Ism-familiya: (.*?)\s*\n" \
                   r"Telefon raqam: (.*?)\s*\nQo’shimcha telefon: (.*?)\s*\nMahsulot/Xizmat/Kurs: (.*?)\s*\n" \
-                  r"Referal manbayi: (.*?)\s*$"
+                  r"Bizni qayerdan eshitdi: (.*?)\s*$"
 
 
 def escape_markdown_v2(text):
