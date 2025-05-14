9b075ac83bb9669a022804cb7b6486a3378981fc
mukhammmad-irmatov <mukhammad.irmatov@gmail.com>
2025-05-14 22:00:18 +0500

    change client_id ->google client id

--- apps/user/views.py
+++ apps/user/views.py
@@ -263,8 +263,8 @@ class GoogleLoginView(APIView):
     def get(self, request):
         google_auth_url = "https://accounts.google.com/o/oauth2/v2/auth"
         params = {
-            "client_id": CLIENT_ID,
-            "redirect_uri": REDIRECT_URI,
+            "client_id": GOOGLE_CLIENT_ID,
+            "redirect_uri": GOOGLE_REDIRECT_URI,
             "response_type": "code",
             "scope": "openid email profile",
             "access_type": "offline",
