f313cd2d7900b0aeec04c5ecb7260610dfaa0f99
ShahzodAbdashev <abdashevshahzod340@gmail.com>
2025-05-16 14:38:01 +0500

    fix: assistant fixed model import

--- apps/integration/models.py
+++ apps/integration/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-from apps.assistant.models import Assistant
 from shared.addons.enums import IntegrationTypes
 from shared.models import BaseModel
 
 
 class Integration(BaseModel):
-    assistant = models.ForeignKey(Assistant, on_delete=models.CASCADE, related_name='integrations')
+    assistant = models.ForeignKey('assistant.Assistant', on_delete=models.CASCADE, related_name='integrations')
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
