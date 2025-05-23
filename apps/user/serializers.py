ac9df2f847b2b1015bcaa4962286edc85bd394e5
mukhammmad-irmatov <mukhammad.irmatov@gmail.com>
2025-05-23 16:41:29 +0500

    remove tkinter import

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -1,4 +1,3 @@
-from tkinter import N
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 from django.utils.translation import gettext_lazy as _
