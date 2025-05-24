ee4ce9d85e10c23213107b32544c730d83c6487b
mukhammmad-irmatov <mukhammad.irmatov@gmail.com>
2025-05-24 09:48:53 +0500

    add messages logging in assistant response

--- apps/shared/addons/utils.py
+++ apps/shared/addons/utils.py
@@ -195,7 +195,7 @@ def get_assistant_response_ai(message, assistant_id, thread_id):
     messages = client.beta.threads.messages.list(
         thread_id=thread_id, order="asc", after=user_message.id
     )
-    print(f"Assistant response: {messages.data}")
+    print(f"Assistant response: {messages.data}, messages: {messages}")
 
     # Get the response text or a fallback message if empty
     assistant_response = messages.data[0].content[0].text.value if messages.data else "No response received."
